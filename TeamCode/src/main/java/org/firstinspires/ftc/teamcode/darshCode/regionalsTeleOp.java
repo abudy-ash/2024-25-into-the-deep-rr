d198480d6345aad39335e08483333b2c60fc51b0
nateybear <nate@schwartzboys.com>
2025-02-19 16:35:43 -0500

    Camera and LED Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/darshCode/regionalsTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/darshCode/regionalsTeleOp.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.darshCode;
 
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -9,8 +11,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
 @TeleOp(name="Regionals TeleOp")
 public class regionalsTeleOp extends OpMode {
+
+    HuskyLens huskyLens;
+
+    //Make sure on the robot confuguration it is set up to leftLights and rightLights
+    private RevBlinkinLedDriver leftLights,rightLights;
+
     // Regular Movement DC Motors
     DcMotorEx backLeftDrive, backRightDrive, frontLeftDrive, frontRightDrive;
 
@@ -23,7 +35,7 @@ public class regionalsTeleOp extends OpMode {
 
     // Sensors
     public IMU imu;
-
+    
     public void darshTeleOp(){
 
         // Movement
@@ -72,6 +84,8 @@ public class regionalsTeleOp extends OpMode {
                         if (gamepad2.left_bumper) {claw.setPosition(0);}
                         if (gamepad2.right_bumper) {claw.setPosition(1);}
 
+        //For Color Detection and updating the LED
+//        colorRecognition();
 
         telemetry.update();
 
@@ -79,6 +93,17 @@ public class regionalsTeleOp extends OpMode {
 
     @Override
     public void init() {
+
+        //Camera
+//        huskyLens = hardwareMap.get(HuskyLens.class,"huskyLens");
+//        huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
+//
+//        //Initialized the LED Lights
+//        leftLights = hardwareMap.get(RevBlinkinLedDriver.class,"leftLights");
+//        rightLights = hardwareMap.get(RevBlinkinLedDriver.class,"rightLights");
+//        blinkinGreen();
+
+
         // Initialization Code
         backRightDrive = hardwareMap.get(DcMotorEx.class, "backRight");
         backLeftDrive = hardwareMap.get(DcMotorEx.class, "backLeft");
@@ -112,6 +137,64 @@ public class regionalsTeleOp extends OpMode {
         telemetry.update();
 
     }
+
+//    public void colorRecognition(){
+//        HuskyLens.Block[] blocks = huskyLens.blocks();
+//        telemetry.addData("Block Count", blocks.length);
+//        for (int i = 0; i < blocks.length; i++) {
+//            int thisColorID = blocks[i].id;
+//
+//            switch(thisColorID){
+//                case 1:
+//                    //Code for if it's Blue (ID = 1)
+//                    telemetry.addData("Color ID","Blue");
+//                    colorDetected = "Blue";
+//                    blinkinBlue();
+//                    break;
+//                case 2:
+//                    //SET UP RED AS ID = 2
+//                    //Code for if it's Red (ID = 2)
+//                    telemetry.addData("Color ID", "Red");
+//                    colorDetected = "Red";
+//                    blinkinRed();
+//                    break;
+//                case 3:
+//                    //SET UP YELLOW AS ID = 3
+//                    //Code for if it's Yellow (ID = 3)
+//                    telemetry.addData("Color ID","Yellow");
+//                    colorDetected = "Yellow";
+//                    blinkinYellow();
+//                    break;
+//            }
+//        }
+//    }
+//
+//    public void blinkinRed(){
+//        leftLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+//        rightLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+//    }
+//    public void blinkinGreen(){
+//        leftLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+//        rightLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+//    }
+//    public void blinkinYellow(){
+//        leftLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
+//        rightLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
+//    }
+//    public void blinkinBlue(){
+//        leftLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+//        rightLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+//    }
+//    public void blinkinBlack(){
+//        leftLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+//        rightLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+//    }
+//
+//    @Override
+//    public void start(){
+//        //For led
+//        blinkinBlack();
+//    }
 
     @Override
     public void loop() {
